1e63d05790ded10e14c5b2c0f327cff9de5a05e4
Jeff Shomali <jeffshomali@gmail.com>
2016-10-23 21:23:50 -0700

    Configuring Database and Path

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Define database configuration
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "PHPForum");
+
+define("SITE_TITLE","Welcome To PHPForum");
+
+//Path Configuration
+define('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/PHPForum/');
+
+?>
