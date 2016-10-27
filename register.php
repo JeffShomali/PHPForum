ac480e82db3310b16b952df3bfefdd5015dbe76b
Jeff Shomali <jeffshomali@gmail.com>
2016-10-26 19:46:03 -0700

    Working With Inner Page Templates

---
+++ register.php
@@ -0,0 +1,10 @@
+<?php require('core/init.php'); ?>
+
+<?php
+//Get Template and Assign Vars
+$template = new Template('templates/register.php');
+
+//Assign variables
+
+//Dsiplay template
+ echo $template;
---
+++ topics.php
@@ -0,0 +1,11 @@
+<?php require('core/init.php'); ?>
+
+<?php
+//Get Template and Assign Vars
+$template = new Template('templates/topics.php');
+
+//Assign variables
+
+
+//Dsiplay template
+ echo $template;
